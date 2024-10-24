72d96b61f92f7535935cc596cd1662e3e100d41f
Eduardo Akira <e.akira@live.com>
2024-10-23 21:12:39 -0300

    feat: adding more withdraw and transfer tests

--- tests/Feature/EventTest.php
+++ tests/Feature/EventTest.php
@@ -91,4 +91,156 @@ class EventTest extends TestCase
         ]);
     }
 
+    /**
+     * Test retrieving the balance of an existing account.
+     */
+    public function test_get_balance_for_existing_account()
+    {
+        $this->postJson('/event', [
+            'type' => 'deposit',
+            'destination' => '100',
+            'amount' => 20,
+        ]);
+
+        $response = $this->getJson('/balance?account_id=100');
+
+        $response->assertStatus(200)
+            ->assertExactJson(20);
+    }
+
+    /**
+     * Test withdrawing from a non-existing account.
+     */
+    public function test_withdraw_from_non_existing_account()
+    {
+        $payload = [
+            'type' => 'withdraw',
+            'origin' => '200',
+            'amount' => 10,
+        ];
+
+        $response = $this->postJson('/event', $payload);
+
+        $response->assertStatus(404)
+            ->assertExactJson(0);
+    }
+
+
+    /**
+     * Test withdrawing from a non-existing account.
+     */
+    public function test_with_invalid_request_in_event_route()
+    {
+        // Sending without the type
+        $payload = [
+            'origin' => '200',
+            'amount' => 10,
+        ];
+
+        $response = $this->postJson('/event', $payload);
+
+        $response->assertStatus(404)
+            ->assertExactJson(0);
+    }
+    /**
+     * Test withdrawing from an existing account.
+     */
+    public function test_withdraw_from_existing_account()
+    {
+        $payload = [
+            'type' => 'withdraw',
+            'origin' => '100',
+            'amount' => 5,
+        ];
+
+        $response = $this->postJson('/event', $payload);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'origin' => [
+                    'id' => '100',
+                    'balance' => 15,
+                ],
+            ]);
+
+        $this->assertDatabaseHas('accounts', [
+            'id' => '100',
+            'balance' => 15,
+        ]);
+    }
+
+    /**
+     * Test transferring from an existing account to another.
+     */
+    public function test_transfer_from_existing_account()
+    {
+        // Perform the transfer
+        $payload = [
+            'type' => 'transfer',
+            'origin' => '100',
+            'amount' => 15,
+            'destination' => '300',
+        ];
+
+        $response = $this->postJson('/event', $payload);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'origin' => [
+                    'id' => '100',
+                    'balance' => 0,
+                ],
+                'destination' => [
+                    'id' => '300',
+                    'balance' => 15,
+                ],
+            ]);
+
+        // Verify that the accounts are updated in the database
+        $this->assertDatabaseHas('accounts', [
+            'id' => '100',
+            'balance' => 0,
+        ]);
+
+        $this->assertDatabaseHas('accounts', [
+            'id' => '300',
+            'balance' => 15,
+        ]);
+    }
+
+    /**
+     * Test transferring from a non-existing account.
+     */
+    public function test_transfer_from_non_existing_account()
+    {
+        $payload = [
+            'type' => 'transfer',
+            'origin' => '200',
+            'amount' => 15,
+            'destination' => '300',
+        ];
+
+        $response = $this->postJson('/event', $payload);
+
+        $response->assertStatus(404)
+            ->assertExactJson(0);
+    }
+
+    /**
+     * Test transferring from an account with insufficient balance.
+     */
+    public function test_transfer_with_insufficient_balance()
+    {
+        $payload = [
+            'type' => 'transfer',
+            'origin' => '100',
+            'amount' => 20,
+            'destination' => '300',
+        ];
+
+        $response = $this->postJson('/event', $payload);
+
+        $response->assertStatus(400)
+            ->assertExactJson(0);
+    }
 }
