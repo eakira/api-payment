f0d28c66339361fd07d8363ece52e549061d5f29
Eduardo Akira <e.akira@live.com>
2024-10-24 07:48:31 -0300

    feat: adding migration for events and accounts

---
+++ database/migrations/2024_10_24_102458_create_events_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->enum('type', ['deposit', 'withdraw', 'transfer'])->index();
+            $table->decimal('balance', 10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
