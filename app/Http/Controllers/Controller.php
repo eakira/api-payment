b3d1dc212b4f55c96764795f324e6de0a60b1cd8
Eduardo Akira <e.akira@live.com>
2024-10-26 07:43:00 -0300

    feat: add respose in controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function response(?array $response, int $status_code = 200)
+    {
+        return response()->json($response, $status_code);
+    }
 }
