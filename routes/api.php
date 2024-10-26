45edb095cbd520005f2d01bbfec40f341569e1cc
Eduardo Akira <e.akira@live.com>
2024-10-26 08:21:17 -0300

    feat: add route to show balance

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::post('reset', [\App\Http\Controllers\ResetController::class, 'reset'])->name('config.reset');
 
+    Route::get('balance', [\App\Http\Controllers\BalanceController::class, 'show'])->name('balance.show');
 });
