ad049997208a9345877973af3548f8e336fc96e9
Eduardo Akira <e.akira@live.com>
2024-10-26 07:52:15 -0300

    feat: add message

---
+++ config/message.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+
+    'success' => [
+        'message' => ["message" => 'The request was successful'],
+        'status_code' => 200
+    ],
+
+    'error' => [
+        'message' => ["message" => 'the server encountered an unexpected condition that prevented it from fulfilling the request'],
+        'status_code' => 500
+    ],
+];
